2ecf4db61f21ea66b2e6a143780a68b096daf780
Praveen Pathmakaran <praveenpathmakaran@gmail.com>
2022-08-30 20:03:06 +0530

    fast laugh page completed

---
+++ lib/presentation/fast_laugh.dart/widgets/video_list_item.dart
@@ -0,0 +1,90 @@
+import 'package:flutter/material.dart';
+import 'package:netflix/core/colors/colors.dart';
+import 'package:netflix/presentation/search/widgets/search_result.dart';
+
+class VideoListItem extends StatelessWidget {
+  final int index;
+  const VideoListItem({Key? key, required this.index}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        Container(
+          color: Colors.accents[index % Colors.accents.length],
+        ),
+        Align(
+          alignment: Alignment.bottomCenter,
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.end,
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                //left side
+                CircleAvatar(
+                  radius: 30,
+                  backgroundColor: Colors.black.withOpacity(0.5),
+                  child: IconButton(
+                    onPressed: () {},
+                    icon: const Icon(
+                      Icons.volume_off,
+                      color: kWhiteColor,
+                      size: 30,
+                    ),
+                  ),
+                ),
+                //right side
+
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: const [
+                    Padding(
+                      padding: EdgeInsets.symmetric(vertical: 10),
+                      child: CircleAvatar(
+                        radius: 30,
+                        backgroundImage: NetworkImage(imageUrl),
+                      ),
+                    ),
+                    VideoActionsWidget(
+                        icon: Icons.emoji_emotions, title: 'LOL'),
+                    VideoActionsWidget(icon: Icons.add, title: 'My List'),
+                    VideoActionsWidget(icon: Icons.share, title: 'Share'),
+                    VideoActionsWidget(icon: Icons.play_arrow, title: 'Play'),
+                  ],
+                )
+              ],
+            ),
+          ),
+        )
+      ],
+    );
+  }
+}
+
+class VideoActionsWidget extends StatelessWidget {
+  final IconData icon;
+  final String title;
+  const VideoActionsWidget({Key? key, required this.icon, required this.title})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 10),
+      child: Column(
+        children: [
+          Icon(
+            icon,
+            color: Colors.white,
+            size: 30,
+          ),
+          Text(
+            title,
+            style: TextStyle(color: kWhiteColor, fontSize: 16),
+          )
+        ],
+      ),
+    );
+  }
+}
