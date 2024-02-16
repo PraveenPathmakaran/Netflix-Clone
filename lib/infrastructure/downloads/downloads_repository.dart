5fb88d6f2769e5e61ce20342d81b04aa465c9d45
Praveen <praveenpathmakaran@gmail.com>
2024-02-16 12:37:07 +0530

    package updated

--- lib/infrastructure/downloads/downloads_repository.dart
+++ lib/infrastructure/downloads/downloads_repository.dart
@@ -1,10 +1,8 @@
-import 'dart:developer';
-
+import 'package:dartz/dartz.dart';
 import 'package:dio/dio.dart';
 import 'package:injectable/injectable.dart';
 import 'package:netflix/domain/core/api_end_points.dart';
 import 'package:netflix/domain/core/failures/main_failure.dart';
-import 'package:dartz/dartz.dart';
 import 'package:netflix/domain/downloads/i_downloads_repo.dart';
 import 'package:netflix/domain/downloads/models/downloads.dart';
 
@@ -19,7 +17,6 @@ class DownloadsRepository implements IDownloadRepo {
           return Downloads.fromJson(e);
         }).toList();
 
-        print(downloadsList);
         return Right(downloadsList);
       } else {
         return const Left(MainFailure.serverFailure());
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,12 +5,12 @@
 import FlutterMacOS
 import Foundation
 
-import path_provider_macos
-import share_plus_macos
-import url_launcher_macos
+import path_provider_foundation
+import share_plus
+import video_player_avfoundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharePlusMacosPlugin.register(with: registry.registrar(forPlugin: "SharePlusMacosPlugin"))
-  UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
+  FVPVideoPlayerPlugin.register(with: registry.registrar(forPlugin: "FVPVideoPlayerPlugin"))
 }
